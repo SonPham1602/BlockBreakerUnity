50b6b20193fe746a0570b8e0b0a9663aa8d1cd25
SonPham1602 <phamngocsonl13.cla@gmail.com>
2019-11-15 00:22:07 +0700

    add controller game play

--- BlockBreakerGame/Assets/Scripts/Block.cs
+++ BlockBreakerGame/Assets/Scripts/Block.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+    Level level;
+    private void Start() {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
     // Start is called before the first frame update
     private void OnCollisionEnter2D(Collision2D other)
     {
-       
+        
         AudioSource.PlayClipAtPoint(breakSound,Camera.main.transform.position);
-       Destroy(gameObject);
+        Destroy(gameObject);
+        level.BlockDestroyed();
+        
    }
 }
---
+++ BlockBreakerGame/Assets/Scripts/Level.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Level : MonoBehaviour
+{
+    SceneLoader sceneLoader;
+    [SerializeField] int breakableBlocks;
+    // Start is called before the first frame update
+    private void Start() {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if(breakableBlocks<=0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+}
