4e01fa3a1e1918bfe47120c0347d853d4541b3c2
SonPham1602 <phamngocsonl13.cla@gmail.com>
2019-11-15 00:01:11 +0700

    Add Sound and level 2

--- BlockBreakerGame/Assets/Scripts/Ball.cs
+++ BlockBreakerGame/Assets/Scripts/Ball.cs
@@ -7,13 +7,16 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddle;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
     
     private Vector2 paddleToBallVector;
+    AudioSource myAudioSource;
     bool hasStarted = false;
     // Start is called before the first frame update
     void Start() 
     {
         paddleToBallVector = transform.position- paddle.transform.position;
+        myAudioSource  = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -38,5 +41,13 @@ public class Ball : MonoBehaviour
             hasStarted = true;
             GetComponent<Rigidbody2D>().velocity = new Vector2(xPush,yPush);
         }
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0,ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
+       
     }
 }
--- BlockBreakerGame/Assets/Scripts/Block.cs
+++ BlockBreakerGame/Assets/Scripts/Block.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSound;
     // Start is called before the first frame update
     private void OnCollisionEnter2D(Collision2D other)
     {
        
-
+        AudioSource.PlayClipAtPoint(breakSound,Camera.main.transform.position);
        Destroy(gameObject);
    }
 }
